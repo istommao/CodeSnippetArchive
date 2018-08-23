682b1ec05bcd587b586973e0fa68224565fd0577
istommao <istommao@gmail.com>
2018-08-23 14:29:27 +0800

    Add index page

--- server/server/urls.py
+++ server/server/urls.py
@@ -16,6 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.shortcuts import render
+
+
+def IndexView(request):
+    return render(request, 'index.html')
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', IndexView)
 ]
