997f0d1ef7336b4933af441a5981847a156c828e
codingcat <istommao@gmail.com>
2021-05-13 17:54:59 +0800

    Add basic project folder

---
+++ main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+
+from src.routers import page
+
+app = FastAPI()
+
+
+app.mount('/static', StaticFiles(directory='static'), name='static')
+
+app.include_router(page.router)
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run('main:app', reload=True, host='127.0.0.1', port=7340)
---
+++ src/routers/page.py
@@ -0,0 +1,28 @@
+"""routers."""
+from typing import Optional
+
+import jinja2
+
+from fastapi import FastAPI, Request, APIRouter
+from fastapi.templating import Jinja2Templates
+
+
+router = APIRouter()
+
+templates = Jinja2Templates(directory='html')
+
+
+@router.get('/')
+async def index_page(request: Request):
+    return templates.TemplateResponse('home.html', {'request': request})
+
+
+@router.get('/tb/{tool_name}/')
+async def tb_page(request: Request, tool_name: str):
+    tpl_name = 'codesnippet/{}.html'.format(tool_name)
+    try:
+        resp = templates.TemplateResponse(tpl_name, {'request': request})
+    except jinja2.exceptions.TemplateNotFound:
+        return templates.TemplateResponse('toolbox/index.html', {'request': request})
+    else:
+        return resp
